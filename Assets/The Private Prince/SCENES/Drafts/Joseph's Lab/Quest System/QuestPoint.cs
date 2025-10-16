700bcd0e8284dcf954ec81896a36fe73e6d8b5e2
Joseph Padel <99005379+AlmightyLoaf03@users.noreply.github.com>
2025-10-16 12:19:40 +0800

    QUEST SYSTEM IMPLEMENTATION (10%)
    - Added a new script and few assets
    - Iidlip lang muna ako hahaha

---
+++ Assets/The Private Prince/SCENES/Drafts/Joseph's Lab/Quest System/QuestPoint.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class QuestPoint : MonoBehaviour
+{
+    [Header("Quest")]
+
+    [SerializeField] private QuestInfoSO questInfoForPoint;
+
+    private bool playerIsNear = false;
+    private string questId;
+    private QuestState currentQuestState;
+
+    private void Awake()
+    {
+        questId = questInfoForPoint.id;
+    }
+
+    private void OnEnable()
+    {
+        GameEventsManager.Instance.questEvents.onQuestStateChange += QuestStateChange;
+        //GameEventsManager.Instance.inputEvents.onSubmitPressed += SubmitPressed;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.Instance.questEvents.onQuestStateChange -= QuestStateChange;
+        //GameEventsManager.Instance.inputEvents.onSubmitPressed -= SubmitPressed; -> I dont have this in my script
+    }
+
+    private void SubmitPressed()
+    {
+        if (!playerIsNear)
+        {
+            return;
+        }
+
+        GameEventsManager.Instance.questEvents.StartQuest(questId);
+        GameEventsManager.Instance.questEvents.AdvanceQuest(questId);
+        GameEventsManager.Instance.questEvents.FinishQuest(questId);
+    }
+
+    private void QuestStateChange(Quest quest)
+    {
+        // Only update the quest state if this point has the corresponding quest
+        if (quest.info.id.Equals(questId))
+        {
+            currentQuestState = quest.state;
+            Debug.Log("Quest with id: " + questId + " update to state: " +  currentQuestState);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            playerIsNear = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            playerIsNear = false;
+        }
+    }
+
+
+}
