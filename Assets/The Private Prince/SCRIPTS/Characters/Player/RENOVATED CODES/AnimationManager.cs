51262e4f5929ea16b757464bdf4e42b3164449b3
Kyah-Ain <196322898+Kyah-Ain@users.noreply.github.com>
2025-12-16 21:38:52 +0800

    Directional Character Movement Created from Scratch

---
+++ Assets/The Private Prince/SCRIPTS/Characters/Player/RENOVATED CODES/AnimationManager.cs
@@ -0,0 +1,26 @@
+using System.Collections; // Grants access to collecitons structures like ArrayLists and Hashtables
+using System.Collections.Generic; // Grants access to collections structures like Lists and Dictionaries
+using UnityEngine; // Grants access to Unity's core features like Datatypes, DateTime, Math, and Debug
+
+public class AnimationManager : MonoBehaviour
+{
+    // ------------------------- VARIABLES -------------------------
+
+    // List of 'Animation State' variables 
+    public enum CharacterState { 
+        isIdle, 
+        isWalking, 
+        isRunning, 
+        isJumping, 
+        isAttacking, 
+        isBlocking 
+    }
+
+    // ------------------------- UNITY METHODS -------------------------
+
+
+
+    // ------------------------- DEV METHODS -------------------------
+
+
+}
---
+++ Assets/The Private Prince/SCRIPTS/Characters/Player/RENOVATED CODES/ThirdPersonController.cs
@@ -0,0 +1,79 @@
+using System.Collections; // Grants access to collecitons structures like ArrayLists and Hashtables
+using System.Collections.Generic; // Grants access to collections structures like Lists and Dictionaries
+using UnityEngine; // Grants access to Unity's core features like Datatypes, DateTime, Math, and Debug
+
+public class ThirdPersonController : MonoBehaviour
+{
+    // ------------------------- VARIABLES -------------------------
+
+    [Header("REFERENCES")]
+    [SerializeField] private CharacterController characController;
+
+    [Header("MOVEMENT")]
+    [SerializeField] private float movementSpeed = 6f;
+    [SerializeField] private float turningSpeed = 0.1f;
+
+    //[SerializeField] private float turningVelocity;
+
+    // ------------------------- UNITY METHODS -------------------------
+
+    // Awake is called before all frame updates
+    private void Awake()
+    {
+        // Evaluates if there's no existing "Character Controller" component on the object
+        if (characController == null)
+        {
+            // Assigns the gameObject's "Character Controller" autmatically to this script
+            characController = GetComponent<CharacterController>();
+
+            Debug.Log($"Character Controller was set: {characController}");
+        }
+        else 
+        {
+            Debug.LogError("ASSIGN A CHARACTER CONTROLLER FIRST BEFORE USING THIS SCRIPT");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Method Calls
+        Move();
+    }
+
+    // ------------------------- DEV METHODS -------------------------
+
+    // Method for Character Movement Logic
+    public void Move() 
+    {
+        // Get's the Horizontal & Vertical Value from Unity's Input System
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Computes for the direction by merging horizontal & vertical positions
+        // - ".normalized" so that moving diagonally would make us not move faster
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // Evaluates if there is a movement
+        // ".magnitude" to compute for the distance 
+        if (direction.magnitude >= 0.1f)
+        {
+            // Computes the angle needed to rotate the character to the direction it's moving
+            // - "Mathf.Atan2" calculates the angle needed to rotate from 0 up to the target x & z coordinate
+            // - "Mathf.Rad2Deg" converts the Rad computed value of "Atan2" into degrees
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            /*
+            // Smooths the character rotation
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turningVelocity,turningSpeed);
+            */
+
+            // Applies the computed rotation to the gameObject's rotation (Rotates the gameObject)
+            // "Quaternion.Euler" to avoid gimbal locking or wrong rotation starting position
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            // Controls the "Character Controller" of a Unity game object
+            characController.Move(direction * movementSpeed * Time.deltaTime);
+        }
+    } 
+}
